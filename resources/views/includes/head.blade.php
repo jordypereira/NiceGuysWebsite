78aa2979d74a1b3581ee59777a5c57285026a746
perjor <jordy.pereira@hotmail.com>
2018-03-01 15:54:08 +0100

    added base layout

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Scotch">
+
+<title>The Nice Guys Website</title>
+
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
