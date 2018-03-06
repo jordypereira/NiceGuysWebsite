1fda44fe9d4503226703e6e52d484f9459a91bd8
perjor <jordy.pereira@hotmail.com>
2018-03-06 16:18:57 +0100

    removed bootstrap cdn and footer copyright adjusted

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,6 @@
-<div id="copyright text-right">© Copyright 2018 The Nice Guys</div>
+<div class="a-copyright">
+    <div class="a-copyright__sign">
+        <span class="fa fa-copyright"></span>
+    </div>
+    <span class="a-copyright__label">2018 The Nice Guys</span>
+</div>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,7 +4,5 @@
 
 <title>The Nice Guys Website</title>
 
-<!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link href="https://cdn.antwerpen.be/core_branding_scss/1.0.0-beta.1/main.min.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="css/app.css">
