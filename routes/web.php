f305b8bf124be2ed4ff541c8ab05172878d01617
Regentix <yves.sterckx@hotmail.com>
2018-04-10 16:34:50 +0200

    Auth routes added

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,27 @@
 |
 */
 
-Route::get('/', 'HomeController@homepage');
+Route::get('/', 'HomeController@index');
 
 Route::get('/admin', 'AdminController@adminpage');
+
+Route::get('/register', 'HomeController@index');
+
+// Authentication Routes...
+$this->get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('admin/login', 'Auth\LoginController@login');
+$this->post('admin/logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('admin/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('admin/register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('admin/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('admin/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('admin/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('admin/password/reset', 'Auth\ResetPasswordController@reset');
+
+// Slug route must be last!
 
 Route::get('/{slug}', 'SlugController@slugpage');
