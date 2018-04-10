11c9cb1dbfa85f5365674de1cb150ff506383cf1
Regentix <yves.sterckx@hotmail.com>
2018-04-10 19:03:46 +0200

    Bootstrap fix

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,5 +4,6 @@
 
 <title>The Nice Guys Website</title>
 
-<link href="https://cdn.antwerpen.be/core_branding_scss/1.0.0-beta.1/main.min.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.antwerpen.be/core_branding_scss/1.0.0-beta.1/main.min.css" type="text/css">
