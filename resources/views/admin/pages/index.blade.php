a6c12829295ec0c64ea244f772977a7d28c8cc8b
Jordy Pereira <jordy.pereira@hotmail.com>
2018-04-25 15:19:01 +0200

    replaced url with dashes, fixes #30

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -21,7 +21,7 @@
             <tr>
                 <td>{{ $page['title'] }}</td>
                 <td>
-                    <a href="/{{ $page['title'] }}" target="_blank"><img src="{{ asset('images/eye.png') }}" alt="Eye icon" title="View"></a>
+                    <a href="/{{ str_replace(' ', '-', $page['title']) }}" target="_blank"><img src="{{ asset('images/eye.png') }}" alt="Eye icon" title="View"></a>
                     <a href="{{ route('pages.edit', $page['id']) }}" class="n-button"><img src="{{ asset('images/pencil.png') }}" alt="Edit icon" title="Edit"></a>
                     <form action="{{ route('pages.destroy', $page['id']) }}" method="POST" class="n-button">
                         @method('DELETE')
