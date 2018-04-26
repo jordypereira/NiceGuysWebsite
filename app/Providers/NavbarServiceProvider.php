d0706b86cc85e84de1b492b594e81e93a2539422
perjor <jordy.pereira@hotmail.com>
2018-04-26 11:25:56 +0200

    Added Navbar Service and added pagelink in nav

---
+++ app/Providers/NavbarServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Page;
+
+class NavbarServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer('*', function ($view) {
+          $pages = Page::all();
+          $view->pages = $pages;
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
     // App\Providers\BroadcastServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\NavbarServiceProvider::class,
     Unisharp\Ckeditor\ServiceProvider::class,
 
 
