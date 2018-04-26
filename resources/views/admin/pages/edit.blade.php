900728fb55b6b68e8fdd0260d2d0586208768f7e
Regentix <yves.sterckx@hotmail.com>
2018-04-26 10:44:29 +0200

    Removed Antwerp classes

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -5,44 +5,42 @@
 @endsection
 
 @section('content')
-    <div class="u-wrapper">
-        <main class="u-container">
-            @if(Session::has('message'))
-                <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
-            @endif
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <h1>Edit page {{ $page['title'] }}</h1>
-            <form method="post" action="{{url('admin/pages', $page['id'] )}}">
-                @method('PUT')
-                @csrf
-                <div class="form-group">
-                    <label for="title">Title:</label>
-                    <input type="text" class="form-control" name="title" id="title" value="{{ $page['title'] }}">
-                </div>
-                <div class="form-group">
-                    <label for="link">Link name:</label>
-                    <input type="text" class="form-control" name="link" id="link">
-                </div>
-                <div class="form-group">
-                    <label for="body">Body:</label>
-                    <textarea class="form-control" name="body" id="body">{{ $page['body'] }}</textarea>
-                </div>
-                <div class="form-group" style="margin-top:60px">
-                    <button type="submit" class="btn btn-success">Submit</button>
-                    <a class="btn btn-danger" href="{{ URL::previous() }}">Decline</a>
-                </div>
-                <script>
-                    CKEDITOR.replace('body');
-                </script>
-            </form>
-        </main>
-    </div>
+    <main class="container">
+        @if(Session::has('message'))
+            <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+        @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <h1>Edit page {{ $page['title'] }}</h1>
+        <form method="post" action="{{url('admin/pages', $page['id'] )}}">
+            @method('PUT')
+            @csrf
+            <div class="form-group">
+                <label for="title">Title:</label>
+                <input type="text" class="form-control" name="title" id="title" value="{{ $page['title'] }}">
+            </div>
+            <div class="form-group">
+                <label for="link">Link name:</label>
+                <input type="text" class="form-control" name="link" id="link">
+            </div>
+            <div class="form-group">
+                <label for="body">Body:</label>
+                <textarea class="form-control" name="body" id="body">{{ $page['body'] }}</textarea>
+            </div>
+            <div class="form-group" style="margin-top:60px">
+                <button type="submit" class="btn btn-success">Submit</button>
+                <a class="btn btn-danger" href="{{ URL::previous() }}">Decline</a>
+            </div>
+            <script>
+                CKEDITOR.replace('body');
+            </script>
+        </form>
+    </main>
 @endsection
