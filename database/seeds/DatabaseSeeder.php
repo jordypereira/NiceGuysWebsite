a4dc32d2bba3ee5e67d1a2c2c61f4f67b751a873
perjor <jordy.pereira@hotmail.com>
2018-04-26 11:38:38 +0200

    added user and page seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+      DB::table('users')->insert([
+        'name' => 'Yves',
+        'email' => 'yves@yves.yves',
+        'password' => bcrypt('yves12'),
+      ]);
+      DB::table('pages')->insert([
+        'title' => 'Een pagina met een veel te lange titel',
+        'link' => 'Pagina',
+        'body' => '<p>Titel</p>',
+      ]);
     }
 }
