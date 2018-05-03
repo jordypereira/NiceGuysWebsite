37e07eeaabc93cc936610776e5252eda7da7e174
Jordy Pereira <jordy.pereira@hotmail.com>
2018-05-03 20:02:02 +0200

    check for isempty

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -21,9 +21,9 @@
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="/admin/pages/create">Add a page</a>
+                            <a class="dropdown-item" href="/admin/pages">View all pages</a>
                             <a class="dropdown-item" href="/admin/home">View all Home Blocks</a>
                             <a class="dropdown-item" href="/admin/home/create">Add a Home Block</a>
-                            <a class="dropdown-item" href="/admin/pages">View all pages</a>
                             <a class="dropdown-item" href="/admin/upload">Upload an image</a>
                             <div class="dropdown-item">
                                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
