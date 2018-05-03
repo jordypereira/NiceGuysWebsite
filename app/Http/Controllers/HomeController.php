9ba07dafc927415bbe679abc483f521805a57743
perjor <jordy.pereira@hotmail.com>
2018-05-03 13:35:48 +0200

    Added header Image to the Home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,6 @@ class HomeController extends Controller
     public function index()
     {
         $blocks = HomeBlock::all();
-        return view('pages/homepage', ['blocks' => $blocks]);
+        return view('pages/homepage', ['blocks' => $blocks, 'headerImage' => 'headerbw.jpg']);
     }
 }
