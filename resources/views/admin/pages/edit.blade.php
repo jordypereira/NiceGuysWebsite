06febbec12f2f4de0344135b2fbfa9c97f6d357b
Jordy Pereira <jordy.pereira@hotmail.com>
2018-05-03 20:34:30 +0200

    wups

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -1,18 +1,18 @@
 @extends('layout')
 
 @section('head')
-    <script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
+    <script src="{{ asset('vendor/unisharp/laravelckeditor/ckeditor.js') }}"></script>
 @endsection
 
 @section('content')
     <main class="container">
         @if(Session::has('message'))
-            <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+            <p class="alert {{ Session::get('alertclass', 'alertinfo') }}">{{ Session::get('message') }}</p>
         @endif
-        @if ($errors->any())
-            <div class="alert alert-danger">
+        @if ($errors>any())
+            <div class="alert alertdanger">
                 <ul>
-                    @foreach ($errors->all() as $error)
+                    @foreach ($errors>all() as $error)
                         <li>{{ $error }}</li>
                     @endforeach
                 </ul>
@@ -22,36 +22,55 @@
         <form method="post" action="{{url('admin/pages', $page['id'] )}}">
             @method('PUT')
             @csrf
-            <div class="form-group">
+            <div class="formgroup">
                 <label for="title">Title:</label>
-                <input type="text" class="form-control" name="title" id="title" value="{{ $page['title'] }}">
+                <input type="text" class="formcontrol" name="title" id="title" value="{{ $page['title'] }}">
             </div>
-            <div class="form-group">
+            <div class="formgroup">
                 <label for="link">Link name:</label>
-                <input type="text" class="form-control" name="link" id="link" value="{{ $page['link'] }}">
+                <input type="text" class="formcontrol" name="link" id="link" value="{{ $page['link'] }}">
             </div>
-            <div class="form-group">
-                <p class="mb-4">Current header image:</p>
-                <div class="gallery-wrapper d-block">
-                    <img class="gallery-image" src="{{ asset('images/'.$headerImage) }}" alt="">
-                </div>
-                <a class="btn btn-outline-dark my-4" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Selecteer een andere foto</a>
-                <div class="collapse multi-collapse" id="multiCollapseExample1">
-                    @foreach($images as $image)
-                        <div class="gallery-wrapper">
-                            <label class="gallery-label" for="image-{{$image["id"]}}"><img class="gallery-image" src="{{ asset('images/header/'.$image["filename"]) }}" alt=""></label>
-                            <input class="gallery-radio" type="radio" name="image" id="image-{{$image["id"]}}" value="{{ $image["filename"] }}">
-                        </div>
-                    @endforeach
-                </div>
+            <div class="formgroup">
+                @if (isset($headerImage))
+                    <p class="mb4">Current header image:</p>
+                    <div class="gallerywrapper dblock">
+                        <img class="galleryimage" src="{{ asset('images/'.$headerImage) }}" alt="">
+                    </div>
+                    <a class="btn btnoutlinedark my4" datatoggle="collapse" href="#multiCollapseExample1" role="button"
+                       ariaexpanded="false" ariacontrols="multiCollapseExample1">Selecteer een andere foto</a>
+                    <div class="collapse multicollapse" id="multiCollapseExample1">
+                        @foreach($images as $image)
+                            <div class="gallerywrapper">
+                                <label class="gallerylabel" for="image{{$image["id"]}}"><img class="galleryimage"
+                                                                                             src="{{ asset('images/header/'.$image["filename"]) }}"
+                                                                                             alt=""></label>
+                                <input class="galleryradio" type="radio" name="image" id="image{{$image["id"]}}"
+                                       value="{{ $image["filename"] }}">
+                            </div>
+                        @endforeach
+                    </div>
+                @else
+                    <label for="image">Header image:</label>
+                    <div>
+                        @foreach($images as $image)
+                            <div class="gallerywrapper">
+                                <label class="gallerylabel" for="image{{$image["id"]}}"><img class="galleryimage"
+                                                                                             src="{{ asset('images/header/'.$image["filename"]) }}"
+                                                                                             alt=""></label>
+                                <input class="galleryradio" type="radio" name="image" id="image{{$image["id"]}}"
+                                       value="{{ $image["filename"] }}">
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
             </div>
-            <div class="form-group">
+            <div class="formgroup">
                 <label for="body">Body:</label>
-                <textarea class="form-control" name="body" id="body">{{ $page['body'] }}</textarea>
+                <textarea class="formcontrol" name="body" id="body">{{ $page['body'] }}</textarea>
             </div>
-            <div class="form-group" style="margin-top:60px">
-                <button type="submit" class="btn btn-success">Submit</button>
-                <a class="btn btn-danger" href="{{ URL::previous() }}">Decline</a>
+            <div class="formgroup" style="margintop:60px">
+                <button type="submit" class="btn btnsuccess">Submit</button>
+                <a class="btn btndanger" href="{{ URL::previous() }}">Decline</a>
             </div>
             <script>
                 CKEDITOR.replace('body');
