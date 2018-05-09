f58b2a1e4544ddd607c8d3238d2ff32953daa24c
Regentix <yves.sterckx@hotmail.com>
2018-05-09 22:42:46 +0200

    Fixed navbar (responsive was broken)

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -6,16 +6,16 @@
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ml-0 neg-p-15 w-100 d-block">
-                <li class="nav-item d-inline-block">
+                <li class="nav-item d-md-block d-lg-inline-block">
                     <a class="nav-link {{(Request::is('/')) ? "active" : ""}}" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
                 </li>
                 @foreach($pages as $page)
-                    <li class="nav-item d-inline-block">
+                    <li class="nav-item d-md-block d-lg-inline-block">
                         <a class="nav-link {{(Request::is(str_replace(' ', '-', $page['link']))) ? "active" : ""}}" href="/{{ str_replace(' ', '-', $page['link']) }}">{{ ucfirst($page['link']) }}</a>
                     </li>
                 @endforeach
                 @auth
-                    <li class="nav-item dropdown d-inline-block float-right">
+                    <li class="nav-item dropdown d-inline-block float-md-none float-lg-right">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Admin actions
                         </a>
