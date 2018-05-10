3ef05bdf7a570f24dfd9501723ac03af8640ebb4
Regentix <yves.sterckx@hotmail.com>
2018-05-10 18:04:03 +0200

    removed carousel on homepage (commented, just in case)

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -65,15 +65,15 @@
             @endif
         @endforeach
     @endif
-    <div class="context-box-1 u-bg-white">
-        <blockquote>
-            <p>Blockquote: Arma virumque cano, Troiae qui primus ab oris Italiam, fato profugus, Laviniaque venit
-                litora, multum ille et terris iactatus et alto vi superum saevae memorem Iunonis ob iram.</p>
-            <p>Multa quoque et bello passus, dum conderet urbem, inferretque deos Latio, genus unde Latinum, Albanique
-                patres, atque altae moenia Romae.</p>
-            <cite>Publius Vergilius Maro</cite>
-        </blockquote>
-        <img src="{{ asset('images/left-arrow.png') }}" alt="left-arrow" class="left-arrow">
-        <img src="{{ asset('images/right-arrow.png') }}" alt="right-arrow" class="right-arrow">
-    </div>
+    {{--<div class="context-box-1 u-bg-white">--}}
+        {{--<blockquote>--}}
+            {{--<p>Blockquote: Arma virumque cano, Troiae qui primus ab oris Italiam, fato profugus, Laviniaque venit--}}
+                {{--litora, multum ille et terris iactatus et alto vi superum saevae memorem Iunonis ob iram.</p>--}}
+            {{--<p>Multa quoque et bello passus, dum conderet urbem, inferretque deos Latio, genus unde Latinum, Albanique--}}
+                {{--patres, atque altae moenia Romae.</p>--}}
+            {{--<cite>Publius Vergilius Maro</cite>--}}
+        {{--</blockquote>--}}
+        {{--<img src="{{ asset('images/left-arrow.png') }}" alt="left-arrow" class="left-arrow">--}}
+        {{--<img src="{{ asset('images/right-arrow.png') }}" alt="right-arrow" class="right-arrow">--}}
+    {{--</div>--}}
 @endsection
