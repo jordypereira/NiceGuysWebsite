125898b7692801e156073ac96eee74508c936d8f
Regentix <yves.sterckx@hotmail.com>
2018-05-17 12:39:26 +0200

    #79

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -21,43 +21,14 @@
             <div class="form-group mt-0 mb-4">
                 @if (isset($headerImage))
                     <label><a target="_blank" href="{{ asset('images/'.$headerImage) }}">Header foto: </a></label>
-                    <div id="accordion">
-                        <button class="btn btn-outline-dark mb-3" type="button" data-toggle="collapse" data-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">Selecteer een foto</button>
-                        <button class="btn btn-outline-dark mb-3" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Upload een foto</button>
-                        @if(count($images) > 0)
-                            <div class="collapse" id="multiCollapseExample1">
-                                @foreach($images as $image)
-                                    <div class="gallery-wrapper">
-                                        <label class="gallery-label" for="image{{$image["id"]}}">
-                                            <img class="gallery-image" src="{{ asset('images/header/'.$image["filename"]) }}" alt="Header Image Gallery">
-                                        </label>
-                                        <div class="text-center">
-                                            <input class="gallery-radio" type="radio" name="image" id="image{{$image["id"]}}" value="{{ $image["filename"] }}">
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        @else
-                            <div class="collapse" id="multiCollapseExample1" style="max-height: 46px" data-parent="#accordion">
-                                <div class="alert alert-danger">
-                                    <p class="m-0">U moet eerst een foto uploaden voor u er een kunt selecteren!</p>
-                                </div>
-                            </div>
-                        @endif
-                        <div class="collapse" id="multiCollapseExample2" style="max-height: 64px" data-parent="#accordion">
-                            <div class="form-group">
-                                <input type="file" class="form-control p-3" name="upload" id="upload">
-                                <input type="hidden" value="header" id="type" name="type">
-                            </div>
-                        </div>
-                    </div>
                 @else
                     <label class="d-block" for="image">Geen header foto geselecteerd (gebruikt default foto):</label>
+                @endif
                     <div id="accordion">
-                        <button class="btn btn-outline-dark my-3" type="button" data-toggle="collapse" data-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Selecteer een foto</button>
-                        <button class="btn btn-outline-dark my-3" type="button" data-toggle="collapse" data-target="#multiCollapseExample4" aria-expanded="false" aria-controls="multiCollapseExample4">Upload een foto</button>
+                        <button class="btn btn-outline-dark mb-3" type="button" data-toggle="collapse" data-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Selecteer een foto</button>
+                        <button class="btn btn-outline-dark mb-3" type="button" data-toggle="collapse" data-target="#multiCollapseExample4" aria-expanded="false" aria-controls="multiCollapseExample4">Upload een foto</button>
                         @if(count($images) > 0)
-                            <div class="collapse">
+                            <div class="collapse" id="multiCollapseExample3">
                                 @foreach($images as $image)
                                     <div class="gallery-wrapper">
                                         <label class="gallery-label" for="image{{$image["id"]}}">
@@ -83,7 +54,6 @@
                             </div>
                         </div>
                     </div>
-                @endif
             </div>
             <div class="form-group mt-0 mb-4">
                 <label for="body">Body:</label>
