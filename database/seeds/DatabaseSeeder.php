de537ba82201edbd104d5145b3d8f182394fc699
perjor <jordy.pereira@hotmail.com>
2018-05-17 14:55:55 +0200

    updated seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -60,37 +60,15 @@ class DatabaseSeeder extends Seeder
 <p>Informatie over <a href="http://www.elfri.be/aanranding-van-de-eerbaarheid">schending van de eerbaarheid</a> (volgens het wetboek).</p>',
       ]);
       DB::table('home_blocks')->insert([
-          'title' => 'Wist u dat..',
+          'title' => 'Strafbaarheid',
           'text' => '
-                <ul>
-                    <li> Ruim een op drie (31,3%) werd al eens uitgescholden
-                        voor slet, hoer, janet… op (weg naar/van) school of in
-                        zijn/haar vrije tijd.
-                    </li>
-                    <li>Bijna een op drie (28,9%) kreeg al eens seksueel
-                        getinte opmerkingen over borsten, billen,
-                        geslachtsdelen op (weg naar/van) school of in
-                        zijn/haar vrije tijd.
-                    </li>
-                    <li>Ruim een op drie (31,3%) werd al eens uitgescholden
-                        voor slet, hoer, janet… op (weg naar/van) school of in
-                        zijn/haar vrije tijd.
-                    </li>
-                    <li>Bijna een op drie (28,9%) kreeg al eens seksueel
-                        getinte opmerkingen over borsten, billen,
-                        geslachtsdelen op (weg naar/van) school of in
-                        zijn/haar vrije tijd.
-                    </li>
-                </ul>'
+                <p>In belgi&euml; deelt men de strafbare feiten op in verschillende gebieden. Als dit gedrag wordt gesteld ten opzichte van minderjarigen, geldt dat als verzwarende omstandigheid en zijn ook de straffen zwaarder.</p>
+
+<p>Klik&nbsp;<a href="/Strafbaarheid">hier</a></p>
+'
       ]);
       DB::table('home_blocks')->insert([
-          'title' => 'Lorem Ipsum',
-          'text' => '
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum. </p>'
+          'video' => 'https://www.youtube.com/embed/HWJLiDkvgQ8'
       ]);
     }
 }
