aa8283a6acad8ad41f97e4d0c62eacded84312ff
Regentix <yves.sterckx@hotmail.com>
2018-05-17 13:19:56 +0200

    #84

--- resources/views/includes/alerts.blade.php
+++ resources/views/includes/alerts.blade.php
@@ -1,12 +1,20 @@
 @if(Session::has('message'))
-    <p class="alert {{ Session::get('alert-class', 'alert-info') }} m-0">{{ Session::get('message') }}</p>
+    <div class="alert alert {{ Session::get('alert-class', 'alert-info') }}">
+        <div class="container">
+            <p class="m-0">{{ Session::get('message') }}</p>
+        </div>
+    </div>
 @endif
 @if ($errors->any())
     <div class="alert alert-danger">
-        <ul class="m-0">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <div class="container">
+            <div class="row">
+                <ul class="m-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 @endif
