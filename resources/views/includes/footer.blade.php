157f5dedfb470d67bcbd594695f8e8968ea4e637
Regentix <yves.sterckx@hotmail.com>
2018-06-02 14:23:03 +0200

    Changed footer icon to svg

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-4">
     <span class="kiddishmedium">© 2018 The Nice Guys</span>
     <button id="footer-button">
-        <img class="footer-icon" src="{{ asset('images/arrow-up.png') }}" alt="">
+        <img class="footer-icon" src="{{ asset('images/arrow-up.svg') }}" alt="">
     </button>
 </footer>
