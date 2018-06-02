d1fd7d3d0c3cc885572c49dc3bb252c84b73b156
perjor <jordy.pereira@hotmail.com>
2018-06-02 13:23:25 +0200

    Can only vote once. Saves in your session on the server. Fixes #111

--- app/Http/Controllers/CountController.php
+++ app/Http/Controllers/CountController.php
@@ -7,7 +7,19 @@ use App\HomeBlock;
 
 class CountController extends Controller
 {
-    public function show($id) {
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function show(Request $request, $id) {
+        if($request->session()->has('counter_'.$id)){
+            return redirect()->to('/');
+        }
+
+        $request->session()->put('counter_'.$id, 'true');
         $homeBlock = HomeBlock::find($id);
         $homeBlock->counter_value += 1;
         $homeBlock->save();
