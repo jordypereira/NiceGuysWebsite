e3f5955336aafa45d628b0510708ee12af3c5212
Regentix <yves.sterckx@hotmail.com>
2018-06-02 14:15:02 +0200

    Updated footer + removed antwerp link in head (aangezien we die toch niet gebruiken

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="o-footer py-5">
-    <span class="o-footer__label kiddishmedium homeblock-title">© 2018 The Nice Guys</span>
-    <button id="footer-button" class="o-footer__button a-button a-button--secondary has-icon d-flex justify-content-center align-items-center h-100">
-        <i class="fa fa-arrow-up"></i>
+<footer class="py-4">
+    <span class="kiddishmedium">© 2018 The Nice Guys</span>
+    <button id="footer-button">
+        <img class="footer-icon" src="{{ asset('images/arrow-up.png') }}" alt="">
     </button>
 </footer>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,5 +4,5 @@
 
 <title>The Nice Guys Website</title>
 
-<link rel="stylesheet" href="https://cdn.antwerpen.be/core_branding_scss/1.0.0-beta.1/main.min.css" type="text/css">
+{{--<link rel="stylesheet" href="https://cdn.antwerpen.be/core_branding_scss/1.0.0-beta.1/main.min.css" type="text/css">--}}
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
