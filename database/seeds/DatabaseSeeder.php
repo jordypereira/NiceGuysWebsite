29517a896bc7e71b831df554c1c1ad90d3a2bb8c
Jordy Pereira <jordy.pereira@hotmail.com>
2018-06-09 18:30:51 +0200

    updated seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,75 +19,143 @@ class DatabaseSeeder extends Seeder
         'password' => bcrypt('niceguys123'),
       ]);
       DB::table('pages')->insert([
-        'title' => 'Wat is strafbaar?',
+        'title' => 'Elke vorm van seksuele intimidatie is strafbaar',
         'link' => 'Strafbaarheid',
-        'color' => '#d0003a',
+        'color' => '#b75300',
         'font_color' => 'white',
         'order' => 1,
-        'body' => '<p>In belgi&euml; deelt men de strafbare feiten op in verschillende gebieden. Als dit gedrag wordt gesteld ten opzichte van minderjarigen, geldt dat als verzwarende omstandigheid en zijn ook de straffen zwaarder.</p>
+        'body' => '<p><strong>Sinds 22 mei 2014 wordt elke vorm van seksuele intimidatie in het openbaar gezien als een strafbaar feit. </strong></p>
 
-<hr />
-<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Aanranding: </strong>Gedwongen seks zonder penetratie: tegen je wil worden betast of de ander moeten aanraken, gedwongen worden om je uit te kleden, jezelf of de ander moet mastruberen, ...</div>
+<p>Vindt u dat u zelf het slachtoffer bent van zulke feiten? Dan is het aangeraden om stappen te ondernemen tegen de dader. Door het probleem niet aan te pakken, wordt voor de dader een gevoel gecre&euml;erd dat deze feiten tolereerbaar zijn en zonder gevolgen kan worden verder gezet.</p>
 
-<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Verkrachting:</strong> Gedwongen seks met penetratie. Volgens de strafwet: &#39;Elke daad van penetratie van welke aard en met welk middel ook, gepleegd op een persoon die daar niet in toestemt&#39;.</div>
+<p>&nbsp;</p>
 
-<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Incest: </strong>Seksueel misbruik door een bloedverwant of door iemand die bij het gezin woont.</div>
+<p><strong>De effectieve straffen:</strong></p>
 
-<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Seksisme:</strong> Gedrag dat bedoeld is om andere mensen te vernederen op basis van zijn/haar geslacht.</div>
-
-<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Prostitutie: </strong>Iemand aanzetten tot prostitutie en het uitbaten van zulke zaken.</div>
-
-<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Grooming:</strong> Als volwassene contact opnemen met een minderjarige om een &#39;vertrouwensband&#39; op te bouwen met het oog op seksueel misbruik.</div>
+<p>Word er effectief gesproken over een strafbaar feit dan kan hier een straf aan worden toe verbonden. Met gevangenisstraf van een maand tot een jaar en met geldboete van vijftig euro tot duizend euro. Als er sprake is van zwaardere feiten zoals aanranding, verkrachting,&hellip; worden er andere en zwaardere straffen van toestand genomen.</p>
 
 <p>&nbsp;</p>
 
 <hr />
-<p><strong><big>Dit zegt het wetboek over de strafbaarheid omtrent seksuele intimidatie:</big></strong></p>
+<p>&nbsp;</p>
 
-<ul>
-    <li>
-    <p><cite><em><strong>Belaging(Strafwetboek Art.442 bis): </strong>Alle gedrag dat de rust van de getroffen persoon ernstig verstoort zoals bijvoorbeeld achternalopen, omringen, intimiderende of beledigende sms-en of berichten op facebook sturen enzovoort.</em></cite></p>
-    </li>
-    <li>
-    <p><cite><em><strong>Voyeurisme (Strafwetboek Art.371): </strong>Personen observeren of doen observeren, maar ook beelden of geluidsopnamen van iemand maken, zonder dat hij/zij dat weet of daarvoor toestemming heeft gegeven.</em></cite></p>
-    </li>
-</ul>
+<h3><strong>Ik wil aangifte doen, hoe doe ik dat?</strong></h3>
 
 <p>&nbsp;</p>
 
-<hr />
-<p><strong>Extra info:</strong></p>
+<p>Bij sprake van grensoverschrijdend gedrag kan er een aangifte worden gedaan bij de politie of kan men contact opnemen met hulporganisaties.</p>
 
-<p>Voor meer informatie omtrent wat wel en niet mag volgens de wet kan men altijd terecht op de pagina van Sensoa: <a href="https://www.allesoverseks.be/themas/seks-grenzen/seks-de-wet">Seks en de wet.</a></p>
+<p>&nbsp;</p>
 
-<p>Informatie over <a href="http://www.elfri.be/aanranding-van-de-eerbaarheid">schending van de eerbaarheid</a> (volgens het wetboek).</p>',
-      ]);
+<p><strong>Aangifte doen bij de politie:</strong></p>
 
-      DB::table('pages')->insert([
-         'title' => 'Wat kan je zelf doen?',
-         'color' => '#d0003a',
-         'font_color' => 'white',
-         'link' => 'Zelf doen',
-         'order' => 2,
-         'body' => '<p>Steeds meer en meer scholieren krijgen te maken met grensoverschrijdend gedrag. Inplaats van deze feiten te vedringen is het verstanding om zelf stappen te kunnen ondernemen.</p>
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Bij nietdringende hulp:&nbsp; Bel de blauwe lijn: 0800 123 12. Maak een onlineafspraak: Politie Antwerpen</div>
 
-<p><strong>Hoe?</strong></p>
+<p>&nbsp;</p>
 
-<p>Aangifte doen bij de politie:</p>
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Bij dringende hulp gelieve het noodnummer 101 te bellen om in contact te komen bij de juiste hulpdiensten.</div>
 
-<ul>
-    <li>Bij dringende hulp gelieve het noodnummer <strong>101</strong> te bellen om in contact te komen bij de juiste hulpdiensten.</li>
-    <li>Bij niet-dringende hulp:&nbsp; Bel de blauwe lijn: <a href="https://www.politieantwerpen.be/">0800 123 12</a>. Maak een online-afspraak: <a href="https://www.politieantwerpen.be/">Politie Antwerpen</a></li>
-</ul>
+<p>&nbsp;</p>
 
-<hr />
-<p>Scholieren kunnen altijd terecht bij andere hulporganisaties:</p>
-
-<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Jong &amp; Van Zin: Organisatie die zorgt voor informatie, participatie en vorming voor kinderen, jongeren en hun begeleiders. Ze hebben een ervaringsgericht vormingsaanbod. <a href="https://www.pimento.be/">(site)</a></div>
+<p><strong>Scholieren kunnen altijd terecht bij andere hulporganisaties:</strong></p>
 
 <div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Sensoa: Sensoa helpt om te praten over relaties, seksualiteit, grensoverschrijdend gedrag, seksuele diversiteit enzovoort. Ze bieden vormingsmateriaal aan en organiseren interessante opleidingen. <a href="https://www.sensoa.be/">(site)</a></div>
 
-<p>&nbsp;</p>'
+<p>&nbsp;</p>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Jong &amp; Van Zin: Organisatie die zorgt voor informatie, participatie en vorming voor kinderen, jongeren en hun begeleiders. Ze hebben een ervaringsgericht vormingsaanbod. <a href="https://www.pimento.be/">(site)</a></div>
+
+<p>&nbsp;</p>
+
+<p><strong>&nbsp;Meerdere hulporganisaties vindt u <a href="http://yves.sterckx.mtantwerp.eu/Contact">hier</a></strong></p>
+',
       ]);
+
+      DB::table('pages')->insert([
+         'title' => 'Iemand noemt mij een hoer, mag dat?',
+         'color' => '#d0003a',
+         'font_color' => 'white',
+         'link' => 'Wat is seksuele intimidatie',
+         'order' => 2,
+         'body' => '<h3><strong>Wat valt allemaal onder seksuele intimidatie?</strong></h3>
+
+<p>&nbsp;</p>
+
+<p><strong>Seksuele intimidatie is een niet-dagdagelijks begrip waardoor dezelfde ervaringen tussen jongeren anders beleefd kunnen worden.<br />
+Hierdoor beleefd niet iedereen dezelfde uitspraken of handelingen intimiderend. Toch worden steeds meer jongeren het slachtoffer van seksuele intimidatie en is het belangrijk om hier actie in te nemen.</strong></p>
+
+<p>Lees wat andere jongeren verstaan onder seksuele intimidatie:</p>
+
+<p>(nog niet afgewerkt)</p>
+
+<blockquote>
+<p>Iemand blijven lastig vallen met intieme vragen - Kaat, 16 jaar</p>
+</blockquote>
+
+<blockquote>
+<p>Mannen die vrouwen met gebaren lastig vallen. - Joke, 17 jaar</p>
+</blockquote>
+
+<p>&nbsp;</p>
+
+<hr />
+<h3>&nbsp;</h3>
+
+<h3><strong>Waar ligt de grens?</strong></h3>
+
+<p>&nbsp;</p>
+
+<p><strong>Ondanks het verschil in beleving, zijn er toch grenzen die kunnen worden opgelegd. Deze zaken worden altijd als seksueel intimiderend beschouwd:</strong></p>
+
+<p>(nog niet afgewerkt)</p>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Seksueel getinte scheldwoorden met de bedoeling om iemand te vernederen.</strong></div>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Iemand die een vrouw een hoer noemt na afwijzing.</div>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Hollebi&rsquo;s benoemen als &lsquo;janet&rsquo;, &lsquo;homo&rsquo;, etc .</div>
+
+<p>&nbsp;</p>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Fysiek (vaak plotse) ongewenste aanrakingen. </strong>Bv. Een onbekende die ongevraagd in de billen van een vrouw knijpt. Bv. Iemand die zijn hand op intieme lichamelijke plaatsen legt.</div>
+
+<p>&nbsp;</p>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>De persoonlijke ruimte niet respecteren. Dit zijn gedragingen waarbij er geen respect wordt getoond voor de comfortzone van een persoon.</strong> Bv. Met een groep rond een vrouw gaan staan. Bv. Onbekende vrouwen filmen met een smartphone.</div>
+
+<p>&nbsp;</p>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Gedragingen die niet gekaderd kunnen worden in de context van &lsquo;versieren&rsquo;.</strong> &nbsp;(vooral van toepassing op onbekende personen) Bv. Seksueel getinte berichten sturen via facebook na dat &lsquo;ontvanger&rsquo; de avances duidelijk heeft afgewezen. Bv. Onbekende vrouwen met een vingerteken proberen dichterbij laten komen.</div>
+
+<p>&nbsp;</p>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px"><strong>Ongewenste gedragingen die zich blijven herhalen.</strong></div>
+
+<p>&nbsp;</p>
+
+<hr />
+<h3>&nbsp;</h3>
+
+<h3><strong>Strafbaarheid</strong></h3>
+
+<p>&nbsp;</p>
+
+<p><strong>Lees alles over strafbaarheid van seksuele intimidatie <a href="http://yves.sterckx.mtantwerp.eu/Strafbaarheid">hier</a></strong></p>
+
+<p>&nbsp;</p>
+
+<p>&nbsp;</p>
+
+<p>&nbsp;</p>
+'
+      ]);
+
+        DB::table('pages')->insert([
+            'title' => 'Heeft u nood aan een gesprek?',
+            'color' => '#2a293a',
+            'font_color' => 'white',
+            'link' => 'Contact',
+            'order' => 3,
+            'body' => '<h2><big>To be continued...</big></h2>' ]);
     }
 }
