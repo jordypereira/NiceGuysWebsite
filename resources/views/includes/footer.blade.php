1514d45e4b92b3825fac0cb57f6a24befece5998
Regentix <yves.sterckx@hotmail.com>
2018-06-10 18:00:06 +0200

    Footer updates

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="py-4">
-    <span class="kiddishmedium">© The Nice Guys</span>
+    <span class="kiddishmedium d-md-none">© The Nice Guys</span>
+    <span class="kiddishmedium d-none d-md-inline-block">© 2018 The Nice Guys</span>
     <button id="footer-button">
         <img class="footer-icon" src="{{ asset('images/arrow-up.svg') }}" alt="">
     </button>
