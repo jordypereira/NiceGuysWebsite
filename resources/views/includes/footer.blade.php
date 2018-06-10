55472a98518c0dcb233fd918556ee8436c2c2b5e
Regentix <yves.sterckx@hotmail.com>
2018-06-10 17:49:55 +0200

    Removed 2018 from footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="py-4">
-    <span class="kiddishmedium">© 2018 The Nice Guys</span>
+    <span class="kiddishmedium">© The Nice Guys</span>
     <button id="footer-button">
         <img class="footer-icon" src="{{ asset('images/arrow-up.svg') }}" alt="">
     </button>
