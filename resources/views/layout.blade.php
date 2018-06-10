7109976ff733394d781724a51e31b1f22d85d9b0
Regentix <yves.sterckx@hotmail.com>
2018-06-10 12:43:43 +0200

    Removed header link to fix bug

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,15 +5,13 @@
     @yield('head')
 </head>
 <body>
-    <a href="{{ route('home') }}">
-        @if (!empty($headerImage))
-            <div class="header" style="background-image: url('{{ asset('images/'.$headerImage) }}')">
-        @else
-            <div class="header" style="background-image: url('{{ asset('images/headerbw.jpg') }}')">
-        @endif
+    @if (!empty($headerImage))
+        <div class="header" style="background-image: url('{{ asset('images/'.$headerImage) }}')">
+    @else
+        <div class="header" style="background-image: url('{{ asset('images/headerbw.jpg') }}')">
+    @endif
             @include('includes.header')
         </div>
-    </a>
     @include('includes.nav')
     @include('includes.alerts')
     @yield('content')
